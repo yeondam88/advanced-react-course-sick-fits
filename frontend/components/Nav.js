39982535516a0ae54534abb70efa8b6f0f53e632
Yeondam Park <giyen9088@gmail.com>
2018-10-12 21:51:46 -0700

    adding components

---
+++ frontend/components/Nav.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Nav = () => (
+  <div>
+    <Link>
+      <a href="/">Home</a>
+    </Link>
+  </div>
+);
+
+export default Nav;
---
+++ frontend/pages/index.js
@@ -0,0 +1,3 @@
+const Home = props => <p>Hime!</p>;
+
+export default Home;
