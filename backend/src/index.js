fb965334d0c1987515350965b9e5b5826472bf1d
Yeondam Park <giyen9088@gmail.com>
2018-10-29 22:11:23 -0700

    adding decode for JWT token

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,17 @@ const db = require("./db");
 
 const server = createServer();
 
-// TODO Use express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
-// TODO Use express middleware to populate current user
+
+// decode the JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
