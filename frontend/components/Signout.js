ca7020695d7a98528b259b284a8c0df85a5c8c8f
Yeondam Park <giyen9088@gmail.com>
2018-11-01 22:19:05 -0700

    signout button

---
+++ frontend/components/Signout.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import { CURRENT_USER_QUERY } from "./User";
+
+const SIGNOUT_MUTATION = gql`
+  mutation SIGNOUT_MUTATION {
+    signout {
+      message
+    }
+  }
+`;
+
+class Signout extends Component {
+  render() {
+    return (
+      <Mutation
+        mutation={SIGNOUT_MUTATION}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+      >
+        {signout => {
+          return <button onClick={signout}>Sign out</button>;
+        }}
+      </Mutation>
+    );
+  }
+}
+
+export default Signout;
