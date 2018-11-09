c366f427d362fa460d23b58ab0662821e9794b9a
Yeondam Park <giyen9088@gmail.com>
2018-11-08 20:54:35 -0800

    adding Signin component

---
+++ frontend/components/PleaseSignin.js
@@ -0,0 +1,7 @@
+import { Query } from "react-apollo";
+import { CURRENT_USER_QUERY } from "./User";
+import { Signin } from "./Signin";
+
+const PleaseSign = props => <p>Please Sign in!</p>;
+
+export default PleaseSign;
