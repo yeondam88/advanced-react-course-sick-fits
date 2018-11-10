ac980bacafcace0bf4b0fb6b06e1f8192d1de365
Yeondam Park <giyen9088@gmail.com>
2018-11-09 22:15:44 -0800

    creating a gated sign in component

--- frontend/components/PleaseSignin.js
+++ frontend/components/PleaseSignin.js
@@ -1,7 +1,22 @@
 import { Query } from "react-apollo";
 import { CURRENT_USER_QUERY } from "./User";
-import { Signin } from "./Signin";
+import Signin from "./Signin";
 
-const PleaseSign = props => <p>Please Sign in!</p>;
+const PleaseSignIn = props => (
+  <Query query={CURRENT_USER_QUERY}>
+    {({ data, loading }) => {
+      if (loading) return <p>Loading...</p>;
+      if (!data.me) {
+        return (
+          <div>
+            <p>Please Sign In before continuing</p>
+            <Signin />
+          </div>
+        );
+      }
+      return props.children;
+    }}
+  </Query>
+);
 
-export default PleaseSign;
+export default PleaseSignIn;
