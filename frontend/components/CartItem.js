6bc7de57d60eb5f2c6e48b122a71a7d28defbb41
Yeondam Park <giyen9088@gmail.com>
2018-11-16 22:23:53 -0800

    displaying cart items and total

---
+++ frontend/components/CartItem.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import formatMoney from "../lib/formatMoney";
+const CartItemStyles = styled.li`
+  padding: 1rem 0;
+  border-bottom: 1px solid ${props => props.theme.lightgrey};
+  display: grid;
+  align-items: center;
+  grid-template-columns: auto 1fr auto;
+  img {
+    margin-right: 10px;
+  }
+  h3,
+  p {
+    margin: 0;
+  }
+`;
+
+const CartItem = props => {
+  const { title, description, image, price } = props.cartItem.item;
+  return (
+    <CartItemStyles>
+      <>
+        <img width="100" src={image} alt={title} />
+        <div className="cart-item-details">
+          <h3>{title}</h3>
+          <p>
+            {formatMoney(price * props.cartItem.quantity)}
+            {" - "}{" "}
+            <em>
+              {props.cartItem.quantity} &times; {formatMoney(price)} each
+            </em>{" "}
+          </p>
+        </div>
+      </>
+    </CartItemStyles>
+  );
+};
+
+CartItem.propTypes = {
+  cartItem: PropTypes.object.isRequired
+};
+export default CartItem;
