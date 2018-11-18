50950d04d000f15cfa3247f7661ca5c202e4a0e3
Yeondam Park <giyen9088@gmail.com>
2018-11-17 20:56:47 -0800

    removing cart items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -239,6 +239,31 @@ const Mutations = {
       },
       info
     });
+  },
+  async removeFromCart(parent, args, ctx, info) {
+    // 1. Find the cart item
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      `{id, user{ id }}`
+    );
+    if (!cartItem) throw new Error("No CartItem found!");
+    // 2. Make sure they own item
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error(`You don't own that item`);
+    }
+    // 3. Delete that cart item
+    return ctx.db.mutation.deleteCartItem(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
   }
 };
 
--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import formatMoney from "../lib/formatMoney";
+import RemoveFromCart from "./RemoveFromCart";
+
 const CartItemStyles = styled.li`
   padding: 1rem 0;
   border-bottom: 1px solid ${props => props.theme.lightgrey};
@@ -21,19 +23,18 @@ const CartItem = props => {
   const { title, description, image, price } = props.cartItem.item;
   return (
     <CartItemStyles>
-      <>
-        <img width="100" src={image} alt={title} />
-        <div className="cart-item-details">
-          <h3>{title}</h3>
-          <p>
-            {formatMoney(price * props.cartItem.quantity)}
-            {" - "}{" "}
-            <em>
-              {props.cartItem.quantity} &times; {formatMoney(price)} each
-            </em>{" "}
-          </p>
-        </div>
-      </>
+      <img width="100" src={image} alt={title} />
+      <div className="cart-item-details">
+        <h3>{title}</h3>
+        <p>
+          {formatMoney(price * props.cartItem.quantity)}
+          {" - "}{" "}
+          <em>
+            {props.cartItem.quantity} &times; {formatMoney(price)} each
+          </em>{" "}
+        </p>
+      </div>
+      <RemoveFromCart id={props.cartItem.id} />
     </CartItemStyles>
   );
 };
