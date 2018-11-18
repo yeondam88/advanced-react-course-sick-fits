b9dc9297c0458677f78839e7228ccad2d3edb764
Yeondam Park <giyen9088@gmail.com>
2018-11-17 21:47:25 -0800

    adding cart item count component

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,6 +4,7 @@ import gql from "graphql-tag";
 import NavStyles from "./styles/NavStyles";
 import User from "./User";
 import Signout from "./Signout";
+import CartCount from "./CartCount";
 import { TOGGLE_CART_MUTATION } from "./Cart";
 
 const Nav = () => (
@@ -26,7 +27,17 @@ const Nav = () => (
             </Link>
             <Signout />
             <Mutation mutation={TOGGLE_CART_MUTATION}>
-              {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+              {toggleCart => (
+                <button onClick={toggleCart}>
+                  My Cart{" "}
+                  <CartCount
+                    count={me.cart.reduce(
+                      (tally, cartItem) => tally + cartItem.quantity,
+                      0
+                    )}
+                  />
+                </button>
+              )}
             </Mutation>
           </>
         )}
