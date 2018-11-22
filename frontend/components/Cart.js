e34cb18d7e3e04ccec4be02df8c9609bfe459098
Yeondam Park <giyen9088@gmail.com>
2018-11-21 21:05:25 -0800

    adding stripe component

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -10,6 +10,7 @@ import SickButton from "./styles/SickButton";
 import CartItem from "./CartItem";
 import formatMoney from "../lib/formatMoney";
 import calcTotalPrice from "../lib/calcTotalPrice";
+import TakeMyMoney from "./TakeMyMoney";
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -55,7 +56,9 @@ const Cart = () => (
           </ul>
           <footer>
             <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-            <SickButton>Checkout</SickButton>
+            <TakeMyMoney>
+              <SickButton>Checkout</SickButton>
+            </TakeMyMoney>
           </footer>
         </CartStyles>
       );
